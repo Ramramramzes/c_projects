20b8069a81f1a6616ff27c47b344b8586bcbff94
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-11-08 16:54:30 +0300

    fix: memory usage

--- my_grep/simplePrint.h
+++ my_grep/simplePrint.h
@@ -8,12 +8,44 @@
 #include <string.h>
 
 #include "findWord.h"
+#include "findStrSize.h"
 
 void simplePrint(int argc,char *argv[]){
 //* Добавим фунцию для поиска поиска слова, которое нужно найти (идет сразу после флага, превое значение без '-')
   char* foundWord = findWord(argc,argv);
+  bool coincidence = false;
+  int needNum = 0;
+  //* Находим индекс нашего текстового содержимого NeedNum;
+  for (int i = 1; i < argc; i++){
+    for (int j = 0; j < (int)strlen(argv[i]); j++){
+      if(foundWord[j] == argv[i][j] && argv[i][0] != '-'){
+        coincidence = true;
+      }else{
+        coincidence = false;
+      }
+    }
+    if(coincidence){
+      needNum = i;
+      break;
+    }
+  }
 
-  printf("%s",foundWord);
+  printf("%d",findStrSize(argc,argv,needNum)); //! Находим максимальную длину строки для создания буфера
+
+//! Исправить ошибку сравнения *foundWord 
+  // for (int i = needNum+1; i < argc; i++){
+  //   FILE *file = fopen(argv[i], "r");
+
+  //   char line[findStrSize(argc,argv,needNum)];  // Максимальная длина строки
+  //   while (fgets(line, sizeof(line), file) != NULL) {
+  //       if (strstr(line, findWord) != NULL) {
+  //           printf("%s", line);
+  //       }
+  //   }
+  // }
+  
+  
+
   free(foundWord);
 }
 
