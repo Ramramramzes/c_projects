9794795c9ea9cba26da69541794c057cbb5f08fd
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-11-08 13:14:16 +0300

    add: s21_grep.c

---
+++ my_grep/s21_grep.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "simplePrint.h"
+
+int main(int argc, char *argv[]){
+    simplePrint(argc,argv);
+  return 0;
+}
+
