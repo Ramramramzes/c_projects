8d87933f59326eb4289e96709d35e97f72b6c654
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-11-09 16:52:14 +0300

    add: lib getopt, del: all functions

--- my_grep/s21_grep.c
+++ my_grep/s21_grep.c
@@ -2,11 +2,52 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
+//todo opt - символ текущей опции
+//todo optarg - указатель на аргумент текущей опции
+//todo optopt - символ неизвестной опции
+//todo optind - индекс следующего аргумента при очередном вызове getopt - указывает на НЕ ОПЦИЮ (АРГУМЕНТ)
 
-#include "simplePrint.h"
+int main(int argc, char *argv[]) {
+  int opt;
+  while ((opt = getopt(argc, argv, "eivcln")) != -1) {
+    switch(opt) {
+      case 'e':
+        printf("Выбран %c\n", opt);
+      break;
+      case 'i':
+        printf("Выбран %c\n", opt);
+      break;
+      case 'v':
+        printf("Выбран %c\n", opt);
+      break;
+      case 'c':
+        printf("Выбран %c\n", opt);
+      break;
+      case 'l':
+        printf("Выбран %c\n", opt);
+      break;
+      case 'n':
+        printf("Выбран %c\n", opt);
+      break;
+      case '?':
+      printf("Неизвестный флаг: %c\n", opt);
+      break;
+    }
+  }
 
-int main(int argc, char *argv[]){
-    simplePrint(argc,argv);
-  return 0;
+  for (int i = optind; i < argc; i++){
+    FILE *file = fopen(argv[i], "r");
+    if(file == NULL){
+      printf("Файла с именем %s не существует\n",argv[i]);
+    }else{
+      fclose(file);
+      printf("argument: %s\n", argv[i]);
+    }
+  }
+    
+  exit(0);
 }
+
+
 
