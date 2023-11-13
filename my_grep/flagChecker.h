7d4c1def33c87132f9d62b8580537b7182392953
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-11-13 17:14:45 +0300

    add: eFlagArr to main.c

--- my_grep/flagChecker.h
+++ my_grep/flagChecker.h
@@ -5,6 +5,7 @@
 //* Создаем структуру с переменными флагов 0 - 1
 typedef struct{
   int e;
+  char **eArgArr;
   int i;
   int v;
   int c;
@@ -21,14 +22,26 @@ char *wordsArr[] = {"e","i","v","c","l","n","h","s","f","o"};
 
 //* Создаем функцию которая на выходе выдаст структуру с результатами флагов
 Flags flagChecker(int argc,char *argv[]){
-  Flags flags = {0,0,0,0,0,0,0,0,0,0};
+  Flags flags = {0, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   //* Обработка флагов 🇷🇺
   int opt;
+  int eLength = 0;
+  for (int i = 0; i < argc; i++){
+    if(strlen(argv[i]) == 2 && argv[i][0] == '-' && argv[i][1] == 'e'){
+      eLength++;
+    }
+  }
+
+  flags.eArgArr = (char**)calloc(eLength + 1, sizeof(char*));
+  int eCounter = 0;
+
   //* Двоеточие вначале флагов - тихий режим, убирает оповещение об ошибках
-  while ((opt = getopt(argc, argv, ":eivclnhsfo")) != -1) {
+  while ((opt = getopt(argc, argv, ":e:ivclnhsfo")) != -1) {
     switch(opt) {
       case 'e':
         flags.e = 1;
+        flags.eArgArr[eCounter] = optarg;
+        eCounter++;
         break;
       case 'i':
         flags.i = 1;
