609ff2a54064236486979567b6cf471067d1df57
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-11-16 16:09:40 +0300

    rebuild: flagChecker(was fatal error)

--- my_grep/flagChecker.h
+++ my_grep/flagChecker.h
@@ -1,11 +1,13 @@
 //* добавляем FLAG_CHECKER_H в предпроцессор 🛠️
 #ifndef FLAG_CHECKER_H
 #define FLAG_CHECKER_H
+#include "findMax.h"
 
 //* Создаем структуру с переменными флагов 0 - 1
 typedef struct{
   int e;
   char **eArgArr;
+  char **fileSearchWords;
   int i;
   int v;
   int c;
@@ -16,64 +18,150 @@ typedef struct{
   int f;
   int o;
   int eCounter;
+  int fileSearchCount;
 } Flags;
 
 //* Создаем массив символов флагов 
 char *wordsArr[] = {"e","i","v","c","l","n","h","s","f","o"};
 
+
 //* Создаем функцию которая на выходе выдаст структуру с результатами флагов
 Flags flagChecker(int argc,char *argv[]){
-  Flags flags = {0, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0,0};
-  //* Обработка флагов 🇷🇺
-  int opt;
+  Flags flags = {0, NULL,NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0};
   int eLength = 0;
+
   for (int i = 0; i < argc; i++){
-    if(strlen(argv[i]) == 2 && argv[i][0] == '-' && argv[i][1] == 'e'){
+    if(strchr(argv[i],'-') && strchr(argv[i],'e')) {
       eLength++;
     }
   }
 
-  flags.eArgArr = (char**)calloc(eLength + 1, sizeof(char*));
+  flags.eArgArr = (char**)calloc(eLength, sizeof(char*));
 
-  //* Двоеточие вначале флагов - тихий режим, убирает оповещение об ошибках
-  while ((opt = getopt(argc, argv, ":e:ivclnhsfo")) != -1) {
-    switch(opt) {
-      case 'e':
-        flags.e = 1;
-        flags.eArgArr[flags.eCounter] = optarg;
-        flags.eCounter++;
-        break;
-      case 'i':
-        flags.i = 1;
-        break;
-      case 'v':
-        flags.v = 1;
-        break;
-      case 'c':
-        flags.c = 1;
-        break;
-      case 'l':
-        flags.l = 1;
-        break;
-      case 'n':
-        flags.n = 1;
-        break;
-      case 'h':
-        flags.h = 1;
-        break;
-      case 's':
-        flags.s = 1;
-        break;
-      case 'f':
-        flags.f = 1;
-        break;
-      case 'o':
-        flags.o = 1;
-        break;
-      case '?':
-        break;
+  //* Обработка флагов 🇷🇺
+  for (int i = 1; i < argc; i++){
+    bool eflagBool = false;
+    //* Флаги идут по одному 1️⃣    
+    if(argv[i][0] == '-' && strlen(argv[i]) == 2){
+      if(argv[i][1] == 'f'){
+        FILE *file = fopen(argv[i+1],"r");
+        //* Выделяем максимальное значение для буфера исходя из всех файлов 
+        char line[findStrSize(argc,argv)];
+        while(fgets(line,sizeof(line),file)){
+          flags.fileSearchCount++;
+          flags.fileSearchWords = (char **)realloc(flags.fileSearchWords, flags.fileSearchCount * sizeof(char *));
+          flags.fileSearchWords[flags.fileSearchCount - 1] = strdup(line);
+        }
+      }
+      switch(argv[i][1]) {
+        case 'e':
+          if(!eflagBool){
+            flags.e = 1;
+            flags.eArgArr[flags.eCounter] = strdup(argv[i+1]);
+            i++;
+            flags.eCounter++;
+            eflagBool = true;
+          }
+          break;
+        case 'i':
+          flags.i = 1;
+          break;
+        case 'v':
+          flags.v = 1;
+          break;
+        case 'c':
+          flags.c = 1;
+          break;
+        case 'l':
+          flags.l = 1;
+          break;
+        case 'n':
+          flags.n = 1;
+          break;
+        case 'h':
+          flags.h = 1;
+          break;
+        case 's':
+          flags.s = 1;
+          break;
+        case 'f':
+          flags.f = 1;
+          break;
+        case 'o':
+          flags.o = 1;
+          break;
+        case '?':
+          break;
+      }
+    }
+    //* Флаги идут пачкой 💯
+    if(argv[i][0] == '-' && strlen(argv[i]) != 2){
+      if(strchr(argv[i],'e') && i < argc ){
+        if(!eflagBool){
+          flags.e = 1;
+          flags.eArgArr[flags.eCounter] = strdup(argv[i+1]);
+          flags.eCounter++;
+          eflagBool = true;
+        }
+      }
+      //*------------ Обработка события -ef добавить break где-нибудь для прерывания
+      bool eBlock = false;
+      bool fBlock = false;
+      for (int j = 1; j < (int)strlen(argv[i]); j++){
+        if(argv[i][j] == 'e'){
+          eBlock = true;
+        }
+        if(argv[i][j] == 'f'){
+          fBlock = true;
+        }
+        if(eBlock && fBlock){
+          printf("Не может быть и f и e в одном флаге");
+        }
+        //*---------------------------------------------------------------------------
+        switch(argv[i][j]) {
+          case 'i':
+            flags.i = 1;
+            break;
+          case 'v':
+            flags.v = 1;
+            break;
+          case 'c':
+            flags.c = 1;
+            break;
+          case 'l':
+            flags.l = 1;
+            break;
+          case 'n':
+            flags.n = 1;
+            break;
+          case 'h':
+            flags.h = 1;
+            break;
+          case 's':
+            flags.s = 1;
+            break;
+          case 'f':
+            flags.f = 1;
+            break;
+          case 'o':
+            flags.o = 1;
+            break;
+          case '?':
+            break;
+        }
+      }
     }
   }
+
+
+  if(flags.e && flags.f){
+    flags.eArgArr = (char **)realloc(flags.eArgArr,flags.fileSearchCount * sizeof(char *));
+    for (int i = 0; i < flags.fileSearchCount; i++){
+      flags.eArgArr[flags.eCounter + i] = flags.fileSearchWords[i];
+    }
+    flags.eCounter += flags.fileSearchCount;
+  }
+  
   return flags;
 }
 
--- my_grep/s21_grep.c
+++ my_grep/s21_grep.c
@@ -3,67 +3,58 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
-//todo opt - символ текущей опции
-//todo optarg - указатель на аргумент текущей опции
-//todo optopt - символ неизвестной опции
-//todo optind - индекс следующего аргумента при очередном вызове getopt - указывает на НЕ ОПЦИЮ (АРГУМЕНТ)
 
-#include "findMax.h"
 #include "flagChecker.h"
-#include "printFunc.h"
 
 int main(int argc, char *argv[]) {
   Flags flags = flagChecker(argc,argv);
-  //* Поиск искомого слова
-  //! Добавить работу с флагом -е/ после каждого -е идет шаблон с которым надо работать/ тут добавить - если флагов -е НЕТУ
-    char *searchWord = NULL;
-    for (int i = optind; i < argc; i++) {
-        if (argv[i][0] != '-') {
-            searchWord = argv[i];
-            optind += 1;
-            break;
-        }
-    }
 
-  //* Обработка аргументов (файлов) 📁
-  char **needArg = NULL; //* Создаем указатель на указатели (массив строк)
-  int needArgCount = 0; //* Переменная для подсчета аргументов
-  char **errorFiles = (char**)malloc(argc * sizeof(char*));
-  int errorSize = 0;
-  //* Добавляем -1 для флага -е 
-  flags.e ? optind -= 1 : false;
-  for (int i = optind; i < argc; i++){
-    FILE *file = fopen(argv[i], "r");
-    if(file == NULL){
-      errorFiles[errorSize] = argv[i];
-      errorSize++;
-      continue;;
-    }else{
-      //! Вспомнить нахрена я делал - 1 (мб после выделения памяти убирал эффект от ++)
-      needArgCount++;
-      needArg = (char **)realloc(needArg, needArgCount * sizeof(char *));
-      needArg[needArgCount - 1] = strdup(argv[i]);  // Копируем строку в массив
-      fclose(file);
-      // printf("argument: %s\n", argv[i]); //* Печать аргументов
+  //* Если флагов нет, первый эллемент будет искомым остальное все обрабатывается как файлы, если нет то в эррор(мб вынести)
+  bool noFlag = true;
+  for (int i = 1; i < argc; i++){
+    if(argv[i][0] == '-'){
+      noFlag = false;
     }
   }
-  
-  int maxFileString = findStrSize(needArgCount,needArg);
-  //!--------------------------------------------------------------------------------------------------------------|
-  printFunc(needArgCount,needArg,maxFileString,searchWord,flags,errorFiles,errorSize); //! Основная функция вывода |
-  //!--------------------------------------------------------------------------------------------------------------|
-  // printf("Максимальная длинна строки файлов - %d\n",maxFileString); //! Вывод максимальной длины строки
-  // printf("Слово для поиска - %s\n",searchWord); //! Вывод слова для поиска
-  
 
-  //* Освобождаем выделенную память
-  for (int i = 0; i < needArgCount; i++) {
-      free(needArg[i]);
+  char** filesFromCom = NULL;
+  int fromComCount = 0;
+  char* searchWord = NULL;
+  //* Если флагов нет
+  if(noFlag){
+    searchWord = argv[1];
+    filesFromCom =(char**)calloc(argc-2,sizeof(char*));
+    for (int i = argc-(argc-2); i < argc; i++){
+      filesFromCom[fromComCount] = argv[i];
+      fromComCount++;
+    }
+  //* Если флаги есть
+  }else{
+    filesFromCom =(char**)calloc(argc,sizeof(char*));
+    if(!flags.e && !flags.f){
+      for (int i = 1; i < argc; i++){
+        if(argv[i][0] != '-' && searchWord == NULL){
+          searchWord = argv[i];
+        }//! Добавить сюда обработку error, добавить массив ошибок для вывода
+      }
+    }
+    for (int i = 1; i < argc; i++){
+      if(argv[i][0] != '-' && argv[i] != searchWord){
+        filesFromCom[fromComCount] = argv[i];
+        fromComCount++;
+      }
+    }
   }
+  // printf("%d\n",flags.fileSearchCount);
+  // printf("%d\n",flags.eCounter);
+  // printf("%s\n",flags.eArgArr[0]);
 
-  free(needArg);
-  exit(EXIT_SUCCESS);
+  for (int i = 0; i < fromComCount ; i++){
+    printf("%s",filesFromCom[i]);
+  }
+  printf("\n%s",searchWord);
 }
+
 
 
 
