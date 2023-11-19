e4a35b685cceeef039d3058a48577362ee308df3
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-11-19 17:18:28 +0300

    add flags C L

--- my_grep/grepPrint.h
+++ my_grep/grepPrint.h
@@ -21,9 +21,9 @@ void grepPrint(Flags flags,int argc, char *argv[],char* patterns[],int patternsL
     char line[maxFileString];
     bool wasPrinted = false;
     int flagV = 0;
-
-    //todo int strCounter = 0;
     int strNumber = 0;
+    int strCounter = 0;
+
     while (fgets(line, sizeof(line) ,file)){
       strNumber++;
       flagV = 0;
@@ -31,14 +31,16 @@ void grepPrint(Flags flags,int argc, char *argv[],char* patterns[],int patternsL
 
       for (int j = 0; j < patternsLen; j++){
         regcomp(&regex, patterns[j], regexFlags);
-
         if(!flags.v){
           if(regexec(&regex, line, 1, pmatch, 0) == 0){
+            strCounter++;
             if(wasPrinted && strcmp(line, prevLine) == 0){
               continue;
             }else{
-              myMainPrint(flags,strNumber,line,files[i],filesLen);
-              wasPrinted = true;
+              if(!flags.c && !flags.l){
+                myMainPrint(flags,strNumber,line,files[i],filesLen);
+                wasPrinted = true;
+              }
             }
           }
         }else{
@@ -51,18 +53,31 @@ void grepPrint(Flags flags,int argc, char *argv[],char* patterns[],int patternsL
       }
 
       if(flagV != patternsLen-1 && flags.v && flagV != 0){
-        myMainPrint(flags,strNumber,line,files[i],filesLen);
+        strCounter++;
+        if(!flags.c && !flags.l){
+          myMainPrint(flags,strNumber,line,files[i],filesLen);
+        }
       }
-
     }
+    if(flags.c || flags.l){
+      myCLprint(flags,strCounter,files[i],filesLen);
+    }
+
 
     fclose(file);
   }
 
 
-  printf("\n");
+  if(!flags.c && !flags.l){
+    printf("\n");
+  }
 
 }
 
 
 #endif
+
+//  if(flags.c || flags.l){
+//         myCLprint(flags,strCounter,files[i],filesLen);
+//         continue;
+//       }
